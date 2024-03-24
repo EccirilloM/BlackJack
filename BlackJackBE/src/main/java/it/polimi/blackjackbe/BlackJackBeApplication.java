0911f5732d72d24fca0c11d16c3715be0451c532
EccirilloM <ettore.cirillo.dev@gmail.com>
2024-03-24 15:23:00 +0100

    push backEnd

---
+++ BlackJackBE/src/main/java/it/polimi/blackjackbe/BlackJackBeApplication.java
@@ -0,0 +1,13 @@
+package it.polimi.blackjackbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackJackBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackJackBeApplication.class, args);
+    }
+
+}
