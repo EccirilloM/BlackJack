4b20b60f239760d5061963f27e6f246081f043b4
Ecirillo <ettore.cirillo.dev@gmail.com>
2024-05-22 20:34:01 +0200

    inizio implementazione testController

--- BlackJackBE/src/test/java/it/polimi/blackjackbe/unit/controller/RicaricaControllerTest.java
+++ BlackJackBE/src/test/java/it/polimi/blackjackbe/unit/controller/RicaricaControllerTest.java
@@ -1,4 +1,22 @@
 package it.polimi.blackjackbe.unit.controller;
 
+import it.polimi.blackjackbe.controller.RicaricaController;
+import it.polimi.blackjackbe.dto.request.RichiestaRicaricaRequest;
+import it.polimi.blackjackbe.service.definition.RicaricaService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@ExtendWith(MockitoExtension.class)
 public class RicaricaControllerTest {
+
+    @Mock
+    private RicaricaService ricaricaService;
+
+    @InjectMocks
+    private RicaricaController ricaricaController;
 }
